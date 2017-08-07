9f03f36e9cdb82c3fec99a16a760a1d0d45c293d
bit-user <bit-user@DESKTOP-JA75RLG>
2017-08-07 13:10:43 +0900

    change commit

--- src/prob1/Sort.java
+++ src/prob1/Sort.java
@@ -1,27 +1,36 @@
 package prob1;
 
 public class Sort {
-	
+
 	public static void main(String[] arg) {
-	
+
 		int array[] = { 5, 9, 3, 8, 60, 20, 1 };
 		int temp = 0;
-		int count =  array.length;
-		
-		System.out.println( "Before sort." );
-		
+		int count = array.length;
+
+		System.out.println("Before sort.");
+
 		for (int i = 0; i < count; i++) {
-			System.out.print( array[ i ] + " " );
+			System.out.print(array[i] + " ");
 		}
-		
-		
+
+		//
+		for (int i = 0; i < count; i++) {
+			for (int j = 0; j < count - 1 - i; j++) {
+				if (array[j] < array[j + 1]) {
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
+
 		//
 		// 정렬 알고리즘이 적용된 코드를 여기에 작성합니다.
-		// 
-	
-		
-		System.out.println( "\nAfter Sort." );
-		
+		//
+
+		System.out.println("\nAfter Sort.");
+
 		for (int i = 0; i < count; i++) {
 			System.out.print(array[i] + " ");
 		}
--- src/prob4/SoundTest.java
+++ src/prob4/SoundTest.java
@@ -7,5 +7,10 @@ public class SoundTest {
 		printSound( new Dog() );
 		printSound( new Sparrow() );
 		printSound( new Duck() );
+		
+		
+	}
+	private static void printSound(Soundable soundable) {
+		System.out.println(soundable.sound());
 	}
 }
